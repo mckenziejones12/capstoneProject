428d5dea25cb3711c87ff6aab1f2e89409e9bfbc
McKenzie Jones <mckenzbeck12@gmail.com>
2024-02-23 15:15:20 -0700

    Add Express, mongoose, and create schemas

---
+++ capstoneExpress/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const { mainModule } = require("process");
+
+const app = express();
+
+// Set up mongoose connection
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+// middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// route handling
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ capstoneExpress/models/Note.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  timestamp: { type: Date },
+  text: { type: String },
+  patientId: { type: Schema.Types.ObjectId, ref: "Patient" },
+});
