17dd6e7c32498ec3921f76e6f3d08f87d9d7161b
McKenzie Jones <mckenzbeck12@gmail.com>
2024-03-25 13:59:36 -0600

    Fix footer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,16 @@
 import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
+import "./Layout.css";
 export const Layout = ({ children }) => {
   return (
-    <>
-      <NavBar />
-      <div>{children}</div>
-      <Footer />
-    </>
+    <div className="contentWrap">
+      <div className="header">
+        <NavBar />
+      </div>
+      <div className="mainContent">{children}</div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
   );
 };
