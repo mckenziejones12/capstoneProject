f5cdf69cf0b81db729239ec40899ea7425163e58
McKenzie Jones <mckenzbeck12@gmail.com>
2024-03-26 20:54:19 -0600

    Add Edit Patient button, Change button display

--- client/src/components/PatientDetailPage.jsx
+++ client/src/components/PatientDetailPage.jsx
@@ -8,25 +8,12 @@ import { useState } from "react";
 import { DeleteModal } from "./DeleteModal";
 
 export const PatientDetailPage = () => {
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  showDeleteModal;
   return (
     <Layout>
       <div id="patientDetailContent">
         <PersonalInfoSection />
         <PatientNotesSection />
       </div>
-      <div id="deletePatientButton">
-        <Button type="delete" onClick={() => setShowDeleteModal(true)}>
-          Delete Patient
-        </Button>
-      </div>
-      <DeleteModal
-        showDeleteModal={showDeleteModal}
-        setShowDeleteModal={setShowDeleteModal}
-      >
-        {"this patient?"}
-      </DeleteModal>
     </Layout>
   );
 };
--- client/src/components/PersonalInfoSection.jsx
+++ client/src/components/PersonalInfoSection.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { DeleteModal } from "./DeleteModal";
 import "./PersonalInfoSection.css";
 
 export const PersonalInfoSection = () => {
   const { patientId } = useParams();
   const [singlePatient, setSinglePatient] = useState(null);
   const [isLoading, setIsLoading] = useState();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  showDeleteModal;
 
   useEffect(() => {
     setIsLoading(true);
@@ -22,40 +26,62 @@ export const PersonalInfoSection = () => {
 
   if (!singlePatient || isLoading) return <div>Loading...</div>;
 
+  const handleEditPatient = () => {
+    console.log("Edit Patient Profile: ");
+  };
+
   return (
     <>
-      <table className="personalInfoTable">
-        <tbody>
-          <tr>
-            <th>First Name: </th>
-            <td>{singlePatient.firstName}</td>
-          </tr>
-          <tr>
-            <th>Last Name: </th>
-            <td>{singlePatient.lastName}</td>
-          </tr>
-          <tr>
-            <th>Street Name: </th>
-            <td>{singlePatient.streetName}</td>
-          </tr>
-          <tr>
-            <th>City: </th>
-            <td>{singlePatient.city}</td>
-          </tr>
-          <tr>
-            <th>State: </th>
-            <td>{singlePatient.state}</td>
-          </tr>
-          <tr>
-            <th>Phone Number: </th>
-            <td>{singlePatient.phoneNumber}</td>
-          </tr>
-          <tr>
-            <th>Date of Birth: </th>
-            <td>{singlePatient.d_birth}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="personalInfoSection">
+        <table className="personalInfoTable">
+          <tbody>
+            <tr>
+              <th>First Name: </th>
+              <td>{singlePatient.firstName}</td>
+            </tr>
+            <tr>
+              <th>Last Name: </th>
+              <td>{singlePatient.lastName}</td>
+            </tr>
+            <tr>
+              <th>Street Name: </th>
+              <td>{singlePatient.streetName}</td>
+            </tr>
+            <tr>
+              <th>City: </th>
+              <td>{singlePatient.city}</td>
+            </tr>
+            <tr>
+              <th>State: </th>
+              <td>{singlePatient.state}</td>
+            </tr>
+            <tr>
+              <th>Phone Number: </th>
+              <td>{singlePatient.phoneNumber}</td>
+            </tr>
+            <tr>
+              <th>Date of Birth: </th>
+              <td>{singlePatient.d_birth}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="personalInfoButtons">
+          <Button type="update" onClick={handleEditPatient}>
+            Edit Patient
+          </Button>
+          <div id="deletePatientButton">
+            <Button type="delete" onClick={() => setShowDeleteModal(true)}>
+              Delete Patient
+            </Button>
+          </div>
+          <DeleteModal
+            showDeleteModal={showDeleteModal}
+            setShowDeleteModal={setShowDeleteModal}
+          >
+            {"this patient?"}
+          </DeleteModal>
+        </div>
+      </div>
     </>
   );
 };
