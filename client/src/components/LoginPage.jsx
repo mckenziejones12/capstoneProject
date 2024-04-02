1eb7ea1d4f8faddd53ec55dbf51accea7aca00e8
McKenzie Jones <mckenzbeck12@gmail.com>
2024-04-01 21:06:19 -0600

    Implement log in function

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,36 +1,62 @@
 import "./LoginPage.css";
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import axios from "axios";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
   const handleCreateNewAccountClick = () => {
     navigate("/register");
   };
+
+  const [userLogin, setUserLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleUsernameInput = (e) => {
+    setUserLogin({ ...userLogin, username: e.target.value });
+  };
+
+  const handlePasswordInput = (e) => {
+    setUserLogin({ ...userLogin, password: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("/api/users", { ...userLogin }).then((response) => {
+      console.log("log in was successfull");
+      navigate("/");
+    });
+  };
+
   return (
     <>
       <div className="loginContent">
         <div className="loginTitle">Capstone Records</div>
         <div className="loginFields">
-          <form className="loginForm" action="">
+          <form className="loginForm" action="" onSubmit={handleSubmit}>
             <div className="loginField">
-              <label className="loginFieldTitle" htmlFor="userNameLogin">
+              <label className="loginFieldTitle" htmlFor="username">
                 Username
               </label>
               <input
                 type="text"
                 className="loginFieldInput"
-                name="userNameLogin"
+                name="username"
+                onChange={handleUsernameInput}
                 required
               />
             </div>
             <div className="loginField">
-              <label className="loginFieldTitle" htmlFor="passwordLogin">
+              <label className="loginFieldTitle" htmlFor="password">
                 Password
               </label>
               <input
                 type="password"
                 className="loginFieldInput"
-                name="passwordLogin"
+                name="password"
+                onChange={handlePasswordInput}
                 required
               />
             </div>
