1015686bd0f2d2e337e5e9722c6069568432136a
McKenzie Jones <mckenzbeck12@gmail.com>
2024-04-21 15:10:31 -0600

    Create UI for 404 error

--- client/src/components/DeleteModal.jsx
+++ client/src/components/DeleteModal.jsx
@@ -1,4 +1,5 @@
 import "./DeleteModal.css";
+import { useState } from "react";
 import { Button } from "./Button";
 import { useParams } from "react-router";
 import { useNavigate } from "react-router";
@@ -8,6 +9,7 @@ export const DeleteModal = ({
   setShowDeleteModal,
   children,
 }) => {
+  const [isUnauthorized, setUnauthorized] = useState(false);
   const { patientId } = useParams();
   const navigate = useNavigate();
 
@@ -15,16 +17,31 @@ export const DeleteModal = ({
     fetch(`/api/users/patients/${patientId}`, {
       method: "DELETE",
     }).then((response) => {
-      navigate("/");
+      if (response.status === 401) {
+        // they are not authorized to do the action
+        // pop up an alert saying not authorized
+        setUnauthorized(true);
+      } else {
+        navigate("/");
+        setUnauthorized(false);
+      }
     });
   };
   if (!showDeleteModal) return null;
 
+  const closeDeleteModal = () => {
+    setShowDeleteModal(false);
+    setUnauthorized(false);
+  };
+
   return (
     <div className="deleteModal">
-      <div id="closeModalBtn" onClick={() => setShowDeleteModal(false)}>
+      <div id="closeModalBtn" onClick={closeDeleteModal}>
         x
       </div>
+      {isUnauthorized && (
+        <div>Warning: you are not authorized to perform this action!</div>
+      )}
       <div>
         This action cannot be undone. Are you sure you want to delete {children}
       </div>
